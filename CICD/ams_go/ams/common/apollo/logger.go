16b59755f3dbad76b786609727e206b83d1b919f
MasayumeChasing <48610123+MasayumeChasing@users.noreply.github.com>
2021-09-17 16:37:02 +0800

    Create logger.go

---
+++ CICD/ams_go/ams/common/apollo/logger.go
@@ -0,0 +1,16 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+// File Name: logger.go
+// Description: Contains log related functions
+
+package apollo
+
+type logger struct {
+}
+
+// Info level logs to print
+func (l *logger) Infof(format string, args ...interface{}) {
+}
+
+// Error level logs to print
+func (l *logger) Errorf(format string, args ...interface{}) {
+}
